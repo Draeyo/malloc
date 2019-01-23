92e3eeb9bb641cc0817c3baaa282c1d4c57d1efc
Draeyo <draeyo@gmail.com>
2019-01-23 13:09:49 +0100

    update allocation with zones and history for free

--- includes/malloc.h
+++ includes/malloc.h
@@ -20,11 +20,11 @@
 # define TINY_SIZE		(size_t)PAGE_SIZE * 2
 # define SMALL_SIZE		(size_t)PAGE_SIZE * 16
 
-# define TINY_DATA_SIZE		(size_t)((TINY_SIZE / 128) - INFO_SIZE)
-# define SMALL_DATA_SIZE	(size_t)((SMALL_SIZE / 128) - INFO_SIZE)
+# define TINY_DATA_SIZE		(size_t)(TINY_SIZE + META SIZE)
+# define SMALL_DATA_SIZE	(size_t)(SMALL_SIZE + META_SIZE)
 
-# define TINY_ZONE_SIZE		(size_t)TINY_DATA_SIZE * 128
-# define SMALL_ZONE_SIZE	(size_t)SMALL_DATA_SIZE * 128
+# define TINY_ZONE_SIZE		(size_t)(TINY__SIZE + INFO_SIZE) * 100
+# define SMALL_ZONE_SIZE	(size_t)(SMALL_SIZE + INFO_SIZE) * 100
 
 # define TINY_TYPE		1
 # define SMALL_TYPE		2
@@ -35,15 +35,27 @@ typedef struct			s_info
 		struct s_info	*next;
 		size_t			size;
 		void			*data;
-		size_t			type;
+		size_t			free;
 }						t_info;
+
+typedef struct			s_each
+{
+		s_each			*next;
+		size_t			type;
+		size_t			size;
+		void			*loc;
+		size_t			left;
+}						t_each;
 
 typedef struct			s_page
 {
 		t_info			*tiny;
+		t_info			*tiny_last;
 		t_info			*small;
+		t_info			*small_last;
 		t_info			*large;
-		t_info			*overall;
+		t_info			*large_last;
+		t_each			overall;
 }						t_page;
 
 t_page					g_mem;
--- srcs/malloc.c
+++ srcs/malloc.c
@@ -1,6 +1,42 @@
 #include "malloc.h"
 #include <stdio.h>
 
+static t_info	*search_space(t_info *alloc, size_t size)
+{
+	t_info		*zone;
+
+	zone = alloc;
+	while (zone)
+	{
+		if (zone->free && zone->size >= size)
+			return (zone);
+		zone = zone->next;
+	}
+	return (NULL)
+}
+
+static void		update_info(t_info **alloc, size_t size)
+{
+	t_info		*new;
+	t_info		*tmp;
+	t_meta		*next;
+
+	new = *alloc;
+	next = new->next ? new->next : NULL;
+	new->free = 0;
+	new->data = (void*)*alloc + INFO_SIZE;
+	if ((new->size - size - INFO_SIZE) > INFO_SIZE)
+	{
+		new->next = (void*)new + size + INFO_SIZE;
+		tmp = new->next;
+		tmp->size = new->size - size - INFO_SIZE;
+		new->size = size;
+		tmp->free = 1;
+		tmp->data = (void*)tmp + INFO_SIZE;
+		tmp->next = next;
+	}
+}
+
 static void		to_free(size_t size, void **addr, size_t type)
 {
 	t_info		*elem;
@@ -22,70 +58,131 @@ static void		to_free(size_t size, void **addr, size_t type)
 		g_mem.overall = elem;
 }
 
+static t_info	*alloc_zone(t_info **zone, size_t size)
+{
+	t_info		*alloc;
+
+	if (!*zone)
+	{
+		if ((*zone = (t_info*)mmap(0, size, PROT, FLAGS, -1, 0)) == MAP_FAILED)
+			return (NULL);
+		(*zone)->size = size - INFO_SIZE;
+		return (*zone);
+	}
+	if ((alloc = (t_info*)mmap(0, size, PROT, FLAGS, -1, 0)) == MAP_FAILED)
+		return (NULL);
+	alloc->size = size - INFO_SIZE;
+	return (alloc);
+}
+
 static void		*alloc_tiny(size_t size)
 {
-	void		*data;
-	//t_info		*tmp_mem;
+	t_info		*alloc;
 
-	if (size > 0)
+	if (!(alloc = search_space(g_mem.tiny, size + INFO_SIZE)))
 	{
-		data = mmap(0, size, PROT, FLAGS, -1, 0);
-		to_free(size, &data, TINY_TYPE);
-/*		tmp_mem = g_mem.tiny;
-		if (tmp_mem)
-		{
-			while (tmp_mem->next)
-				tmp_mem = tmp_mem->next;
-			tmp_mem->next = tmp;
-		}
-		else
-			g_mem.tiny = tmp;*/
-		return (data);
+		alloc = alloc_zone(&(g_mem.tiny), TINY_ZONE_SIZE);
+		update_info(&alloc, size);
+		if (g_mem.tiny_last)
+			g_mem.tiny_last->next = alloc;
+		g_mem.tiny_last = alloc;
+		return (alloc->data);
 	}
-	return (NULL);
+	update_info(&alloc, size);
+	g_mem.tiny_last = alloc;
+	return (alloc->data);
 }
 
 static void		*alloc_small(size_t size)
 {
-	void		*data;
-//	t_info		*tmp_mem;
+	t_info		*alloc;
 
-	if (size > 0)
+	if (!(alloc = search_space(g_mem.small, size + INFO_SIZE)))
 	{
-		data = mmap(0, size, PROT, FLAGS, -1, 0);
-		to_free(size, &data, SMALL_TYPE);
-//		tmp_mem = g_mem.small;
-//		if (tmp_mem)
-//		{
-//			while (tmp_mem->next)
-//				tmp_mem = tmp_mem->next;
-//			tmp_mem->next = tmp;
-//		}
-//		else
-//			g_mem.small = tmp;
-		return (data);
+		alloc = alloc_zone(&(g_mem.small), SMALL_ZONE_SIZE);
+		update_info(&alloc, size);
+		if (g_mem.small_last)
+			g_mem.small_last->next = alloc;
+		g_mem.small_last = alloc;
+		return (alloc->data);
 	}
-	return (NULL);
+	update_info(&alloc, size);
+	g_mem.small_last = alloc;
+	return (alloc->data);
 }
 
 static void		*alloc_large(size_t size)
 {
-	void		*data;
+	t_info		*alloc;
 
-	if (size > 0)
+	if ((alloc = search_space(g_mem.large, size)))
 	{
-		data = mmap(0, size, PROT, FLAGS, -1, 0);
-		to_free(size, &data, LARGE_TYPE);
-		return (data);
+		alloc->next = NULL;
+		alloc->free = 0;
+		alloc->size = size - INFO_SIZE;
+		alloc->data = alloc + 1;
+		g_mem.large_last = alloc;
+		return (alloc->data);
 	}
-	return (NULL);
+	if ((alloc = (t_info*)mmap(0, size, PROT, FLAGS, -1, 0)) == MAP_FAILED)
+		return (NULL);
+	alloc->next = NULL;
+	alloc->free = 0;
+	alloc->size = size - INFO_SIZE;
+	alloc->data = alloc + 1;
+	if (g_mem.large_last)
+		g_mem.large_last-> = alloc;
+	g_mem.large_last = alloc;
+	if (!g_mem.large)
+		g_mem.large = alloc;
+	return (alloc->data);
+}
+
+static void		set_each(t_each **last, size_t size, void *loc)
+{
+	t_each		*tmp;
+
+	if ((tmp = (t_each*)mmap(0, TINY_ZONE, PROT, FLAGS, -1, 0)) == MAP_FAILED)
+		return (NULL);
+	if (*last)
+		(*last)->next = tmp;
+	tmp->next = NULL;
+	tmp->size = size;
+	tmp->loc = loc;
+	tmp->left = TINY_ZONE_SIZE - sizeof(t_each);
+	*last = tmp;
+	return ((void*)tmp);
+}
+
+static	void	set_info(size_t size, void *loc)
+{
+	static t_each	*last = NULL;
+	t_each			*tmp;
+
+	if (!g_mem.each)
+	{
+		g_mem.each = set_each(&last, size, loc);
+		return ;
+	}
+	if (last->left - sizeof(t_each) <= 0)
+	{
+		set_each(&last, size, loc);
+		return ;
+	}
+	tmp = last + 1;
+	tmp->next = NULL;
+	tmp->size = size;
+	tmp->loc = loc;
+	tmp->left = last->left - sizeof(t_each);
+	last->next = tmp;
+	last = tmp;
 }
 
 void	*malloc(size_t size)
 {
 	void	*ret;
 
-	if (size <= 0)
+	if (size <= 0 || size >= SIZE_MAX)
 		return (NULL);
 	else if (size <= TINY_SIZE)
 		ret = alloc_tiny(size);
